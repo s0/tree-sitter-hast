d4348dda3a5e46c4f70a0b2b15e0507a4b032764
Sam Lanning <sam@samlanning.com>
2019-06-14 23:02:02 -0700

    Add type definitions for HAST

---
+++ src/highlight.ts
@@ -0,0 +1,6 @@
+import {Tree} from 'tree-sitter';
+import { HastElement } from './hast';
+
+export function highlightTree(_tree: Tree): HastElement | null {
+  return null;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,1 @@
-console.log("hello world");
-
+export {PreparedLanguage, PreparedLanguages, loadLanguagesFromPackage} from './prepare-language';
